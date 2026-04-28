6935268383c0f0cb3498561595da2d05eade72a0
bcsaldias <bcsaldias@uc.cl>
2026-04-27 22:56:46 -0700

    Removed extra space

--- script.js
+++ script.js
@@ -24,9 +24,6 @@ const SKETCH_SCRIPT_BY_ID = {
     sk14: 'sketches/sketch14.js',
     sk16: 'sketches/sketch16.js',
     sk17: 'sketches/sketch17.js',
-    sk18: 'sketches/sketch18.js',
-    sk19: 'sketches/sketch19.js',
-    sk20: 'sketches/sketch20.js',
     sk15: 'sketches/sketch15-hwk5.js',
 };
 
